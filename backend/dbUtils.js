9817dc6fbba0b4490703dd2355db1e86f8753829
FireBlackHat <firebot1020@gmail.com>
2021-02-16 20:58:10 +0330

    Backend DB Hotfix #2

--- backend/dbUtils.js
+++ backend/dbUtils.js
@@ -2,7 +2,10 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
 
