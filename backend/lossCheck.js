76e5305b403ead3634554fd11bbe5a495ca53574
FireBlackHat <firebot1020@gmail.com>
2021-02-16 22:33:04 +0330

    Frontend Hotfix #2

--- backend/lossCheck.js
+++ backend/lossCheck.js
@@ -33,46 +33,48 @@ exports.lossCheck = async (CheckInterval) => {
             .then(async (res) => await res.json())
             .catch((err) => console.log(err));
 
-          oneInchOutcome = ethers.utils.formatEther(
-            ethers.BigNumber.from(oneInchQuery.toTokenAmount)
-          );
+          if (oneInchQuery.toTokenAmount) {
+            oneInchOutcome = ethers.utils.formatEther(
+              ethers.BigNumber.from(oneInchQuery.toTokenAmount)
+            );
 
-          let OutcomeDiff = oneInchOutcome - exchangeOutcome;
-          let outcomeDiffPercent = (OutcomeDiff / exchangeOutcome) * 100;
+            let OutcomeDiff = oneInchOutcome - exchangeOutcome;
+            let outcomeDiffPercent = (OutcomeDiff / exchangeOutcome) * 100;
 
-          if (
-            exchangeOutcome &&
-            exchangeOutcome < oneInchOutcome &&
-            outcomeDiffPercent > 1
-          )
-            await LossesDB.create({
-              swapExchange: "uniswap",
-              transactionid: swap.transaction.id,
-              sender: await fetch(
-                `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${swap.transaction.id}&apikey=${process.env.ETHERSCAN_API_KEY}`,
-                {
-                  method: "GET",
-                  retryOn: async (attemp, error, response) => {
-                    if (attemp > 1) return false;
+            if (
+              exchangeOutcome &&
+              exchangeOutcome < oneInchOutcome &&
+              outcomeDiffPercent > 1
+            )
+              await LossesDB.create({
+                swapExchange: "uniswap",
+                transactionid: swap.transaction.id,
+                sender: await fetch(
+                  `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${swap.transaction.id}&apikey=${process.env.ETHERSCAN_API_KEY}`,
+                  {
+                    method: "GET",
+                    retryOn: async (attemp, error, response) => {
+                      if (attemp > 1) return false;
 
-                    var json = await response.json();
-                    if (!json.status) return true;
-                  },
-                  retryDelay: 10 * 10 ** 3,
-                }
-              )
-                .then(async (res) => await res.json())
-                .catch((error) => console.log(error))
-                .then((jsonres) => jsonres.result.from),
-              fromToken: swap.pair.token0.symbol,
-              toToken: swap.pair.token1.symbol,
-              exchangeIncome: exchangeIncome,
-              exchangeOutcome: exchangeOutcome,
-              oneInchOutcome: oneInchOutcome,
-              OutcomeDiff: OutcomeDiff,
-              OutcomeDiffPercent: outcomeDiffPercent.toFixed(2),
-              timestamp: swap.timestamp,
-            });
+                      var json = await response.json();
+                      if (!json.status) return true;
+                    },
+                    retryDelay: 10 * 10 ** 3,
+                  }
+                )
+                  .then(async (res) => await res.json())
+                  .catch((error) => console.log(error))
+                  .then((jsonres) => jsonres.result.from),
+                fromToken: swap.pair.token0.symbol,
+                toToken: swap.pair.token1.symbol,
+                exchangeIncome: exchangeIncome,
+                exchangeOutcome: exchangeOutcome,
+                oneInchOutcome: oneInchOutcome,
+                OutcomeDiff: OutcomeDiff,
+                OutcomeDiffPercent: outcomeDiffPercent.toFixed(2),
+                timestamp: swap.timestamp,
+              });
+          }
         }
       } catch (error) {
         console.log(error);
